aa466fe19bd0a4fa8e4fa1c478c43130e7e32f6a
seancarro11 <relicplayswitch@gmail.com>
2026-02-06 10:55:47 -0500

    Camera Follows Player

--- Assets/Code/FollowPlayer.cs
+++ Assets/Code/FollowPlayer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject player;
+    public Vector3 offset = new Vector3(0, 0, -10);
     void Start()
     {
         
@@ -13,6 +16,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
